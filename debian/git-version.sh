b30b63d83b3840612686d5de54314090d92c0cad
Brett Johnson <brett@linuxturtle.net>
2018-08-06 21:05:43 -0600

    merge new packaging from d-u

--- debian/git-version.sh
+++ debian/git-version.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# Script to output a version string with the number of commits since the common
-# ancestor appended to the manually hardcoded API version
-BUILD_COMMIT_ID=upstream-unstable
+# Script to output a version string with the number of commits since the base
+# commit (i.e. the commit which started the CURRENT_BASE release)
 
-CURRENT_BASE="2018.0-dev"
+# Optional argument: the .git directory to use
+
+CURRENT_BASE="2018.0~dev"
 # There are no tags, so manually hardcode commit ID where CURRENT_BASE was created.
 CURRENT_BASE_COMMIT="dbb0f14bcda1fc42a7cd9a3dd8d78d797b2d5448"
 
-NB_OF_COMMITS=`git log --oneline $CURRENT_BASE_COMMIT..$BUILD_COMMIT_ID | wc -l`
+NB_OF_COMMITS=`git --git-dir $1 log --oneline $CURRENT_BASE_COMMIT..HEAD | wc -l`
 echo "$CURRENT_BASE+$NB_OF_COMMITS"
