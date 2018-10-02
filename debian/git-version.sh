9b9dff74a69e8ae2183740489be75ad73a4012b0
Brett Johnson <brett@linuxturtle.net>
2018-10-01 18:51:18 -0600

    merge debian packaging

---
+++ debian/git-version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Script to output a version string with the number of commits since the base
+# commit (i.e. the commit which started the CURRENT_BASE release)
+
+# Optional argument: the .git directory to use
+
+CURRENT_BASE="2018.0~dev"
+# There are no tags, so manually hardcode commit ID where CURRENT_BASE was created.
+CURRENT_BASE_COMMIT="dbb0f14bcda1fc42a7cd9a3dd8d78d797b2d5448"
+
+NB_OF_COMMITS=`git --git-dir $1 log --oneline $CURRENT_BASE_COMMIT..HEAD | wc -l`
+echo "$CURRENT_BASE+$NB_OF_COMMITS"
